3ffb34d19d3b6ab8a86008cedc67a3ac6d53bee3
irands0220 <167028698+irands0220@users.noreply.github.com>
2025-12-03 14:29:36 -0700

    update

--- Analysis/RandForestDGK.R
+++ Analysis/RandForestDGK.R
@@ -3,7 +3,6 @@ library(tidymodels)
 library(embed)
 library(vroom)
 library(workflows)
-library(glmnet)
 library(ranger)
 library(themis)
 
@@ -15,52 +14,40 @@ testData <- vroom("DontGetKicked/OG_Download/test.csv", show_col_types = FALSE)
 
 # --- RECIPE ---
 my_recipe <- recipe(IsBadBuy ~ ., data = trainData) %>%
-  step_string2factor(all_nominal_predictors()) %>%    
-  step_impute_mode(all_nominal_predictors()) %>%      
-  step_impute_median(all_numeric_predictors()) %>%    
-  step_other(all_nominal_predictors(), threshold = 0.005) %>% 
+  step_string2factor(all_nominal_predictors()) %>%
+  step_impute_mode(all_nominal_predictors()) %>%
+  step_impute_median(all_numeric_predictors()) %>%
+  step_other(all_nominal_predictors(), threshold = 0.005) %>%
   step_normalize(all_numeric_predictors())
 
 
-# --- MODEL SPEC ---
-rf_bal <- rand_forest(
-  mtry = tune(),
-  min_n = tune(),
-  trees = 500) %>%
+# --- RANDOM FOREST MODEL (NO TUNING) ---
+rf_model <- rand_forest(
+  mtry = 7,
+  min_n = 10,
+  trees = 100
+) %>%
   set_mode("classification") %>%
-  set_engine("ranger")
+  set_engine("ranger", importance = "impurity")
+
 
 # --- WORKFLOW ---
-balanced_workflow <- workflow() %>%
-  add_model(rf_bal) %>%
-  add_recipe(my_recipe)
+rf_workflow <- workflow() %>%
+  add_recipe(my_recipe) %>%
+  add_model(rf_model)
 
-# --- GRID ---
-tuning_grid <- grid_regular(mtry(range=c(1, 9)), min_n(), levels = 3)
 
-# --- FOLDS ---
-folds <- vfold_cv(trainData, v = 10, repeats = 1)
-
-# --- TUNE ---
-cv_results <- tune_grid(
-  balanced_workflow,
-  resamples = folds,
-  grid = tuning_grid,
-  metrics = metric_set(roc_auc)
-)
-
-best_tune <- cv_results %>% select_best(metric = "roc_auc")
-
-# --- FINAL FIT ---
-rand_for_fit <- balanced_workflow %>%
-  finalize_workflow(best_tune) %>%
+# --- FIT FINAL MODEL ---
+rf_fit <- rf_workflow %>%
   fit(data = trainData)
 
-# --- PREDICT ---
-preds <- predict(rand_for_fit, new_data = testData, type = "prob") %>%
+
+# --- PREDICT ON TEST SET ---
+preds <- predict(rf_fit, new_data = testData, type = "prob") %>%
   bind_cols(testData %>% select(RefId)) %>%
   select(RefId, .pred_1) %>%
   rename(IsBadBuy = .pred_1)
 
 
-vroom_write(x=preds, file="./NaiveBayes_predictions.csv", delim=",")
+# --- WRITE SUBMISSION FILE ---
+vroom_write(preds, "RF_predictions.csv", delim = ",")
